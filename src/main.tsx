2bbb7a45027ee2c84b95747cf6c9da698fb28e55
abroudoux <arthur.broudoux@gmail.com>
2023-12-11 10:28:08 +0100

    create navbar & import shadcn theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster } from 'sonner';
+import { Toaster } from "sonner";
 
 import { ThemeProvider } from "@/config/theme-provider";
 
 import Home from "@/pages/Home";
+import Profile from "@/pages/Profile";
+import Auth from "@/pages/Auth";
+import Courses from "@/pages/Courses";
+
+import Navbar from "@/components/Nav/Navbar";
 
 import "@/style/index.css";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement,).render(
@@ -14,8 +20,12 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement,
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />}></Route>
+                <Route path="/courses" element={<Courses />}></Route>
+                <Route path="/profile" element={<Profile />}></Route>
+                <Route path="/auth" element={<Auth />}></Route>
             </Routes>
+            <Navbar/>
             <Toaster richColors />
         </BrowserRouter>
-  </ThemeProvider>,
+    </ThemeProvider>
 );

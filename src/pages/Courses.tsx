d99aae260d833235b616011a312f40dadeba43df
abroudoux <arthur.broudoux@gmail.com>
2023-12-11 13:27:34 +0100

    add avatar

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,5 +1,7 @@
 export default function Courses() {
     return (
-        <section className="page">Courses</section >
+        <section className="page">
+            <h1 className="text-3xl mb-10">Courses</h1>
+        </section >
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { CourseCard } from "@/components/CourseCard/CourseCard";
 export default function Home() {
 	return (
 		<section className="page">
-			<h1 className="text-3xl">Recommendations</h1>
+			<h1 className="text-3xl mb-10">Recommendations</h1>
 			<CourseCard topic="code" title="Listes ul/ol" description="Appenez à utiliser les listes en HTML" duration="30min" level="Facile"/>
 		</section>
   	);

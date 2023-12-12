1db2776e94aa4b5b268b898ed27f592f7e4ff87d
abroudoux <arthur.broudoux@gmail.com>
2023-12-12 12:00:00 +0100

    add link sections profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faCheck, faPodcast } from "@fortawesome/free-solid-svg-icons";
 
@@ -5,6 +6,7 @@ import { ModeToggle } from "@/components/ui/mode-toggle";
 
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
 
 
 export default function Profile() {
@@ -22,25 +24,25 @@ export default function Profile() {
             </div>
             <h1 className="text-3xl mb-5">Hello, { username }</h1>
             <div className="w-full h-auto flex-row-center gap-2 mb-2">
-                <div className="w-48 h-48 bg-[#361530] rounded-2xl relative hover:cursor-pointer hover:rounded-3xl transition-all">
+                <Link to="/profile/favourites" className="w-48 h-48 bg-[#361530] rounded-2xl relative hover:cursor-pointer hover:rounded-3xl transition-all">
                     <FontAwesomeIcon icon={faHeart} className="text-[#F08CCC] text-5xl p-3 absolute top-0 right-0" />
                     <p className="text-[#F08CCC] bottom-0 absolute p-3 text-3xl font-base">
                         Favoris
                     </p>
-                </div>
-                <div className="w-48 h-48 bg-[#172F22] rounded-2xl relative hover:cursor-pointer hover:rounded-3xl transition-all">
+                </Link>
+                <Link to="/profile/lessons" className="w-48 h-48 bg-[#172F22] rounded-2xl relative hover:cursor-pointer hover:rounded-3xl transition-all">
                     <FontAwesomeIcon icon={faCheck} className="text-[#65E0AA] text-5xl p-3 absolute top-0 right-0" />
                     <p className="text-[#65E0AA] bottom-0 absolute p-3 text-3xl font-base">
                         Terminées
                     </p>
-                </div>
+                </Link>
             </div>
-            <div className="w-full h-48 bg-[#0F3263] rounded-2xl relative hover:cursor-pointer hover:rounded-3xl transition-all">
+            <Link to="/podcasts" className="w-full h-48 bg-[#0F3263] rounded-2xl relative hover:cursor-pointer hover:rounded-3xl transition-all">
                 <FontAwesomeIcon icon={faPodcast} className="text-[#71B7F9] text-5xl p-3 absolute top-0 right-0" />
                 <p className="text-[#71B7F9] bottom-0 absolute p-3 text-3xl font-base">
                     Podcasts
                 </p>
-            </div>
+            </Link>
         </section>
     );
 };
